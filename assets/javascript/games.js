bd638300755d694d25603ff5a220c4a611c405dc
C Howe <brook.theis@gmail.com>
2018-10-06 19:05:05 -0600

    Cleaned up comments

--- assets/javascript/games.js
+++ assets/javascript/games.js
@@ -1,16 +1,20 @@
 
-
+//Possible guesses for computer
 var alphabet = ["a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z"];
-// this variable will be assigned one of the three letters
+
+//Randomize computer guess
 var computerChoice = alphabet[Math.floor(Math.random() * alphabet.length)];
-//this array will hold what we guess
+
+//this array will hold what the user guesses
 var userGuesses = [];
+
 //This is the counter for wins and losses
 var wins = 0;
 var losses = 0;
 
 //This is what we'll use to count down
 var guessesLeft = 9;
+
 //this will reset the game
 var Reset = function () {
     computerChoice = alphabet[Math.floor(Math.random() * alphabet.length)];
@@ -19,27 +23,28 @@ var Reset = function () {
 }
 
 
-    
-//Three functions to update guesses, update guesses left, and update guesses so far
 
-//this function will capture the keyboard click, and it's going to eliminate the guesses left by one
+//this function will capture the key pressed
 document.onkeyup = function(event) {
     var userChoice = event.key.toLowerCase();
     console.log("Computer choice:" + computerChoice);
     console.log("User choice: " + userChoice);
 
+//add user guesses to array
     userGuesses.push(userChoice);
     console.log("User guesses:" + userGuesses);
 
+//decrease guesses left 
     guessesLeft--
     console.log("Guesses left: " + guessesLeft)
 
+//search user guesses array for Computer Choice
     var answerCheck = userGuesses.find(function(element) {
         return element == computerChoice;
       });
     console.log("Answer check: " + answerCheck);
 
-    
+//Update wins and losses and reset game
     if (guessesLeft === 0 && computerChoice == answerCheck) {
         wins++;
         console.log("Wins: " + wins);
@@ -50,7 +55,8 @@ document.onkeyup = function(event) {
         console.log("Losses: " + losses);
         Reset();
     }
-
+    
+//Add text to html
     document.getElementById("userChoice-text").textContent = "You guessed: " + userChoice;
     document.getElementById("guessesLeft-text").textContent = "You have " + guessesLeft + " guesses left.";
     document.getElementById("guessesMade-text").textContent = "Your guesses so far: " + userGuesses;
@@ -58,8 +64,7 @@ document.onkeyup = function(event) {
     document.getElementById("losses-text").textContent = "Losses: " + losses;
 }
 
-// if 
-//this function will be called when we reset everything
+
 
 
 
